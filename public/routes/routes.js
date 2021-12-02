021efac59d952f823a4ccfdfc8ddae7ed2b1da9f
Newton Hoang <newtonhoang99@gmail.com>
2021-12-01 19:00:43 -0800

    restructuring for heroku deployment

--- public/routes/routes.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express').Router();
-
-const apiRoutes = require('./api.js');
-const htmlRoutes = require('./htmlDir.js');
-
-Router.use(apiRoutes);
-Router.use(htmlRoutes);
-
-module.exports = Router;
